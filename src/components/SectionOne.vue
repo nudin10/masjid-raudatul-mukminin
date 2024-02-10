775470e792e45cf791a10a0f8a8fa92a35639907
Nudin <ahmadazhar10@gmail.com>
2024-02-11 00:38:46 +0800

    Fallback when api fails

--- src/components/SectionOne.vue
+++ src/components/SectionOne.vue
@@ -1,6 +1,5 @@
 <template>
-    <div
-        id="pengenalan"
+    <div id="pengenalan"
         class="bg-default-img bg-cover bg-center min-h-screen min-w-full max-h-screen-xl max-w-screen-xl font-default text-black pt-20 px-14">
         <div class="mt-10 w-1/3 h-[40em] flex flex-row align-middle">
             <div class="h-full w-full bg-white-translucent rounded-xl">
@@ -43,15 +42,24 @@ let ayatData = ref({
 
 onMounted(
     async () => {
-        fetchRetry(arabic, 500, 5).then(res => res.json()).then(data => {
-            ayatData.value.text.arabic = data.data.text
-            ayatData.value.metadata.surahName = data.data.surah.name
-            ayatData.value.metadata.surahNameEng = data.data.surah.englishName
-            ayatData.value.metadata.number = data.data.numberInSurah
-        })
-        fetchRetry(english, 500, 5).then(res => res.json()).then(data => ayatData.value.text.english = data.data.text)
+        try {
+            fetchRetry(arabic, 500, 3).then(res => res.json()).then(data => {
+                ayatData.value.text.arabic = data.data.text
+                ayatData.value.metadata.surahName = data.data.surah.name
+                ayatData.value.metadata.surahNameEng = data.data.surah.englishName
+                ayatData.value.metadata.number = data.data.numberInSurah
+            })
+            fetchRetry(english, 500, 5).then(res => res.json()).then(data => ayatData.value.text.english = data.data.text)
+        } catch (error) {
+            console.log("Failed to get API response: "+err)
+        ayatData.value.text.arabic = "إِنَّمَا یَعۡمُرُ مَسَـٰجِدَ ٱللَّهِ مَنۡ ءَامَنَ بِٱللَّهِ وَٱلۡیَوۡمِ ٱلۡـَٔاخِرِ وَأَقَامَ ٱلصَّلَوٰةَ وَءَاتَى ٱلزَّكَوٰةَ وَلَمۡ یَخۡشَ إِلَّا ٱللَّهَۖ فَعَسَىٰۤ أُو۟لَـٰۤىِٕكَ أَن یَكُونُوا۟ مِنَ ٱلۡمُهۡتَدِینَ"
+        ayatData.value.text.english = "Only he should visit or tend God's houses of worship who believes in God and the Last Day, and is constant in prayer, and spends in charity, and stands in awe of none but God: for [only such as] these may hope to be among the right-guided!"
+        ayatData.value.metadata.surahName = "At-Tawba"
+        ayatData.value.metadata.surahNameEng = ""
+        ayatData.value.metadata.number = "18"
+
+        }
     }
 );
-
 
 </script>
