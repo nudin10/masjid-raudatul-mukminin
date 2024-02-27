65a6a7f8b898bc9e865c99d8f15fed6aa623286d
Nudin <ahmadazhar10@gmail.com>
2024-02-27 21:30:17 +0800

    try change path

--- src/components/ImageSlider2.vue
+++ src/components/ImageSlider2.vue
@@ -9,12 +9,12 @@ import { FwbCarousel } from 'flowbite-vue'
 // import img3 from '../assets/images/masjid-pagi.jpg'
 
 const pictures = [
-  {src: 'src/assets/images/jemaah.jpg', alt: 'Image 1'},
-  {src: 'src/assets/images/2022-07-19.jpg', alt: 'Image 2'},
-  {src: 'src/assets/images/masjid-pagi.jpg', alt: 'Image 3'},
-  {src: 'src/assets/images/2022-11-03.jpg', alt: 'Image 4'},
-  {src: 'src/assets/images/2023-06-16.jpg', alt: 'Image 5'},
-  {src: 'src/assets/images/2023-12-16.jpg', alt: 'Image 6'},
+  {src: '/src/assets/images/jemaah.jpg', alt: 'Image 1'},
+  {src: '/src/assets/images/2022-07-19.jpg', alt: 'Image 2'},
+  {src: '/src/assets/images/masjid-pagi.jpg', alt: 'Image 3'},
+  {src: '/src/assets/images/2022-11-03.jpg', alt: 'Image 4'},
+  {src: '/src/assets/images/2023-06-16.jpg', alt: 'Image 5'},
+  {src: '/src/assets/images/2023-12-16.jpg', alt: 'Image 6'},
 ]
 
 </script>
