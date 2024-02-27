ef83e5f03bbd387908b5f194d9b2af3b81cd09b5
Nudin <ahmadazhar10@gmail.com>
2024-02-27 23:29:51 +0800

    try direct import

--- src/components/ImageSlider2.vue
+++ src/components/ImageSlider2.vue
@@ -4,17 +4,18 @@
 
 <script setup>
 import { FwbCarousel } from 'flowbite-vue'
-// import img from '../assets/images/jemaah.jpg'
-// import img2 from '../assets/images/default-wp.jpg'
-// import img3 from '../assets/images/masjid-pagi.jpg'
+import img1 from '../assets/images/img-1.jpg'
+import img2 from '../assets/images/img-2.jpg'
+import img3 from '../assets/images/img-3.jpg'
+import img4 from '../assets/images/img-4.jpg'
 
 const pictures = [
   {src: 'src/assets/images/jemaah.jpg', alt: 'Image 1'},
-  {src: 'src/assets/images/img-1.jpg', alt: 'Image 2'},
+  {src: img1, alt: 'Image 2'},
   {src: 'src/assets/images/masjid-pagi.jpg', alt: 'Image 3'},
-  {src: 'src/assets/images/img-2.jpg', alt: 'Image 4'},
-  {src: 'src/assets/images/img-3.jpg', alt: 'Image 5'},
-  {src: 'src/assets/images/img-4.jpg', alt: 'Image 6'},
+  {src: img2, alt: 'Image 4'},
+  {src: img3, alt: 'Image 5'},
+  {src: img4, alt: 'Image 6'},
 ]
 
 </script>
